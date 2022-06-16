3ee16640447dfa856cd2afeb9ccc1b41f6494b0c
muanziri <munaziribienaime@yahoo.com>
2022-06-16 16:55:49 +0000

    now thrbackend

---
+++ googleDrive.js
@@ -0,0 +1,46 @@
+
+const { google } = require('googleapis');
+const key= require('./duterestory-ecc42c3b6063.json')
+var drive = google.drive("v3");
+var jwToken = new google.auth.JWT(
+    key.client_email,
+    null,
+    key.private_key, ["https://www.googleapis.com/auth/drive"],
+    null
+  );
+  jwToken.authorize((authErr) => {
+    if (authErr) {
+      console.log("error : " + authErr);
+      return;
+    } else {
+      console.log("Authorization accorded");
+    }
+  });
+  const totheDrivers= (fileMetadata,media,stringedFilePath,user,folderId)=>{
+    drive.files.create({
+     auth: jwToken,
+     resource: fileMetadata,
+     media: media,
+     fields: 'id'
+   }, function(err, file) {
+     if (err) {
+       // Handle error
+       console.error(err);
+     } else {
+      
+       console.log('File Id: ', file.data.id);
+       new UserRecording({
+         AudioId:file.data.id,
+         UserName:user.userName,
+         ProfilePic:user.ProfilePhotoUrl,
+         FolderId:folderId
+       }).save().then((results)=>{
+         console.log('he is saved')
+       })
+       fs.unlink(stringedFilePath,()=>{
+         console.log("deleted")
+       })
+     }
+   });
+   }
+   module.exports=totheDrivers;
--- index.js
+++ index.js
@@ -1,13 +1,147 @@
 const express=require('express')
 const mongoose=require('mongoose')
+const cookieSession=require('cookie-session')
+const flash=require('flash')
+const passport=require('passport')
+const multer=require('multer')
+const upload=multer();
+const Readable = require('stream').Readable; 
+const {totheDrivers}=require('./googleDrive')
 
+
+const DB="mongodb+srv://DutereStory:DutereStory@cluster0.zo6ti8b.mongodb.net/DutereStory1?retryWrites=true&w=majority";
+mongoose.connect(DB,{ useNewUrlParser:true,useUnifiedTopology:true})
+  .then((results)=>{
+
+    
+   console.log('connected....');
+  })
+  .catch((err)=>{
+      console.warn(err)
+  })
+ 
+function bufferToStream(buffer) { 
+  var stream = new Readable();
+  stream.push(buffer);
+  stream.push(null);
+
+  return stream;
+}
 const app=express()
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
+app.use(cookieSession({
+    name: 'session',
+    keys: ['MUNAMUNAMUNA'],
+  
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }))
+  app.use(flash())
+app.set('view engine','ejs');
+app.use('/static',express.static(__dirname+'/static'))
+app.use(express.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+}));
 
 app.get('/',(req,res)=>{
     res.render('index')
 })
+app.post('/ToTheDrive',upload.any(), (req,res)=>{
+    
+      const user=req.user
+    let files=req.files
+    let filepath="./audioUploads/";
+    let originalname=files[0].originalname+'.aac'
+    let stringedFilePath=filepath+originalname;
+   var folderId = "1WhwVTQycr7uyO2r_kiGPE38VunkC-njB";
+  var fileMetadata = {
+        'name': [originalname],
+        parents: [folderId]
+      };
+      var media = {
+            mimeType: 'audio/aac',
+           body: bufferToStream(req.file.buffer)
+          };  
+          
+   totheDrivers(fileMetadata,media,stringedFilePath,user,folderId)
+    
+    
+})
+
+app.post('/PodcastControl',(req,res)=>{
+  var jwToken = new google.auth.JWT(
+      key.client_email,
+      null,
+      key.private_key, ["https://www.googleapis.com/auth/drive"],
+      null
+    );
+    jwToken.authorize((authErr) => {
+      if (authErr) {
+        console.log("error : " + authErr);
+        return;
+      } 
+    });
+
+    const uploadToTheDriveMakeFOlder= (fileMetadata)=>{
+      drive.files.create({
+        auth: jwToken,
+        resource: fileMetadata,
+        fields: 'id'
+      }, function(err, file) {
+        if (err) {
+          // Handle error
+          console.error(err);
+        } else {
+          let fileId=file.data.id
+          req.flash('message','Now Record the first espode');
+          req.flash('Id',fileId);
+          req.flash('playName',req.body.PlayName);
+          let name=req.body.PlayName;
+          let Season=req.body.Season;
+          let discrition=req.body.SeasonDiscription
+         new genesis({
+           PlayNames:name,
+           seasonNumber:Season,
+           PlayDiscription:discrition,
+           FolderId:fileId
+         }).save().then((results)=>{
+         }).catch((err)=>{
+             if (err) throw err;
+         })
+          res.redirect('/PodcastControl')
+          
+          
+        }
+      });
+      }
+  
+
+
+var folderId = "1WFFcWOU-EvMGWhp7_SSlsaXdp-e5dSEs";
+var folderName=req.body.PlayName+" S "+req.body.Season   
+var fileMetadata = {
+      'name': folderName,
+      'mimeType': 'application/vnd.google-apps.folder',
+      parents: [folderId]
+     };
+     function funct(fileId){
+      console.log(fileId)
+     }
+    })
+
+
+uploadToTheDriveMakeFOlder(fileMetadata) 
 const port= process.env.port||3300
 app.listen(port,()=>{
     console.log('heard from port')
