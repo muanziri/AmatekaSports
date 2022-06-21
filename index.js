788ec13081f2ec0c6bfba20c2ee4715586c8663c
muanziri <munaziribienaime@yahoo.com>
2022-06-21 16:16:24 +0000

    fhfh

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 app.use(session({
   secret: 'somethingsecretgoeshere',
-  resave: false,
-  saveUninitialized: true,
+  resave: true,
+  saveUninitialized: false,
   cookie: { secure: true }
 }));
   app.use(flash())
