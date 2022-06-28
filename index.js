34d79fbeb781ca2de5fad96d5597382ddf2c8b5d
muanziri <munaziribienaime@yahoo.com>
2022-06-28 09:34:20 +0000

    hero

--- index.js
+++ index.js
@@ -66,12 +66,15 @@ app.get('/auth/google/success', (req, res) => {
   res.redirect('/')
 })
 app.get('/', (req, res) => {
+  if(req.user){
   paymentYear.find({PhoneNumber:req.user.PhoneNumber}).then((paymentres)=>{
     paymentMonth.find({PhoneNumber:req.user.PhoneNumber}).then((paymentres2)=>{
       paymentWeek.find({PhoneNumber:req.user.PhoneNumber}).then((paymentres3)=>{
       res.render('index', { user: req.user,paymentYear:paymentres,paymentMonth:paymentres2,paymentWeek:paymentres3})
    }) })})
-
+  }else{
+    res.render('index',{user:req.user})
+  }
      
 })
 app.post('/addLikes', (req, res) => {
