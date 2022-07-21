bd2141de252480dfcb93785336d99ffd901135dd
muanziri <munaziribienaime@yahoo.com>
2022-07-21 07:32:45 +0000

    kjnkjxkas askjnas askjnas

--- index.js
+++ index.js
@@ -1066,17 +1066,17 @@ app.post('/flutterWaveWithDraw', (req, res) => {
     paymentMonth.find({tx_ref:kid}).then((paymentres2)=>{
       paymentWeek.find({tx_ref:kid}).then((paymentres3)=>{
        if (paymentres.length >0){
-      let ceck=amount-paymentres[0].CashLeft
+      let ceck=paymentres[0].CashLeft-amount
         if (ceck>0){
           console.log('no theft',ceck)
         }else{console.log('a thief')}
     }else if(paymentres2.length >0){
-      let ceck=amount-paymentres2[0].CashLeft
+      let ceck=paymentres2[0].CashLeft-amount
       if (ceck>0){
         console.log('no theft',ceck)
       }else{console.log('a thief')}
     }else if(paymentres3.length >0){
-     let  ceck=amount-paymentres3[0].CashLeft
+     let  ceck=paymentres3[0].CashLeft-amount
      if (ceck>0){
       console.log('no theft',ceck)
     }else{console.log('a thief')}
